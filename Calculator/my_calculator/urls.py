e1e278ee36487222119fbd34aeec057787ac35ee
Md Mahedi Hasan <148598797+MHBOKUL@users.noreply.github.com>
2025-03-12 11:01:45 +0600

    Delete Calculator directory

--- Calculator/my_calculator/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import calculator_view
-
-urlpatterns = [
-    path('', calculator_view, name='my_calculator'),
-]
--- Calculator/my_calculator/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def calculator_view(request):
-    return render(request, 'index.html')
